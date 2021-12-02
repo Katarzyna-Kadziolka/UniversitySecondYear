79d73989c3dfdac1fa2c73eb37921af6d3bf11fc
Katarzyna-Kadziolka <kasiakadziolka@gmail.com>
2021-12-02 18:48:12 +0100

    Lancuch zobowiazan i polecenie

---
+++ UniversitySecondYear/LancuchZobowiazan/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Text;
+
+public interface Lancuch{
+    void ustawNastepne(Lancuch c);
+    void przetwarzaj(Powiadomienia powiadomienia);
+}
+ 
+public class Powiadomienia{
+  
+    private int number;
+  
+    public Powiadomienia(int number){
+        this.number = number;
+    }
+  
+    public int pobierzLiczbe(){
+        return number;
+    }
+ 
+}
+ 
+public class BrakLancuch : Lancuch{
+  
+    private Lancuch nastepneWLancuchu;
+
+    public void ustawNastepne(Lancuch c){
+        nastepneWLancuchu = c;
+    }
+
+    public void przetwarzaj(Powiadomienia powiadomienia){
+        if (powiadomienia.pobierzLiczbe() <= 0){
+            Console.WriteLine("Brak powiadomień");
+        }else{
+            ustawNastepne(new MaloPowiadomienLancuch());
+            nastepneWLancuchu.przetwarzaj(powiadomienia);
+        }
+    }
+}
+
+public class MaloPowiadomienLancuch : Lancuch{
+  
+    private Lancuch nastepneWLancuchu;
+
+    public void ustawNastepne(Lancuch c){
+        nastepneWLancuchu = c;
+    }
+
+    public void przetwarzaj(Powiadomienia powiadomienia){
+        if (powiadomienia.pobierzLiczbe() <= 5){
+            Console.WriteLine($"Mało powiadomień: {powiadomienia.pobierzLiczbe()}");
+        }else{
+            ustawNastepne(new DuzoPowiadomienLancuch());
+            nastepneWLancuchu.przetwarzaj(powiadomienia);
+        }
+    }
+}
+
+public class DuzoPowiadomienLancuch : Lancuch{
+  
+    public void ustawNastepne(Lancuch c){}
+
+    public void przetwarzaj(Powiadomienia powiadomienia){
+        Console.WriteLine($"Dużo powiadomień: {powiadomienia.pobierzLiczbe()}");
+    }
+}
+
+ 
+class Program{
+    static void Main(String[] args){
+        Console.OutputEncoding = Encoding.UTF8;
+        Lancuch l1 = new BrakLancuch();
+
+        int i =  0;
+        l1.przetwarzaj(new Powiadomienia(i));
+        i++;
+        l1.przetwarzaj(new Powiadomienia(i));
+        i = i + 11;
+        l1.przetwarzaj(new Powiadomienia(i));
+        i = i - 9;
+        l1.przetwarzaj(new Powiadomienia(i));
+        i = i - 3;
+        l1.przetwarzaj(new Powiadomienia(0));
+    
+    }
+}
---
+++ UniversitySecondYear/Polecenie/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Text;
+
+public interface IPolecenie{
+    void wykonaj();
+}
+
+public class KomendaWlacz : IPolecenie {
+    Lampa _lampa;
+    public KomendaWlacz(Lampa lampa) {
+        _lampa = lampa;
+    }
+    public void wykonaj() {
+        _lampa.wlacz();
+    }
+}
+
+public class KomendaWylacz : IPolecenie {
+    Lampa _lampa;
+    public KomendaWylacz(Lampa lampa) {
+        _lampa = lampa;
+    }
+    public void wykonaj() {
+        _lampa.wylacz();
+    }
+}
+  
+
+public class Lampa{
+    public void wylacz(){
+        stan = false;
+    }
+
+    public void wlacz(){
+        stan = true;
+    }
+    private bool stan = false;
+    
+    public bool sprawdz(){
+        return stan;
+    }
+}
+
+
+public class Pilot{
+    private IPolecenie _polecenie;
+    public void ustawPolecenie(IPolecenie polecenie) {
+        _polecenie = polecenie;
+    }
+    public void wcisnijGuzik() {
+        _polecenie.wykonaj();
+    }
+}
+
+
+class Program{
+    static void Main(string[] args)    {
+        Lampa lampa = new Lampa();
+        Console.OutputEncoding = Encoding.UTF8;
+        IPolecenie wlacz = new KomendaWlacz(lampa);
+        IPolecenie wylacz = new KomendaWylacz(lampa);
+        Pilot pilot = new Pilot();
+        /* UZUPEŁNIĆ */
+    
+        Console.WriteLine(lampa.sprawdz() ? "Lampa włączona" : "Lampa wyłączona");
+    
+        pilot.ustawPolecenie(wlacz);
+        pilot.wcisnijGuzik();
+        Console.WriteLine(lampa.sprawdz() ? "Lampa włączona" : "Lampa wyłączona");
+    
+        pilot.ustawPolecenie(wylacz);
+        pilot.wcisnijGuzik();
+        Console.WriteLine(lampa.sprawdz() ? "Lampa włączona" : "Lampa wyłączona");
+    
+    }
+}
